7c6097f76ecb7f77fdd5321450f3e7cf522ce395
bluegecko <kei09doya@gmail.com>
2025-04-29 13:35:37 +0900

    Modularize DirEntry logic into a new `entry` module
    Extracted the `DirEntry` and related logic from `main.rs` to a new `entry.rs` module for better code organization and maintainability. Updated relevant imports and function calls to reflect this modularization.

---
+++ src/entry.rs
@@ -0,0 +1,95 @@
+use std::path;
+use std::path::Path;
+use anyhow::anyhow;
+use walkdir::WalkDir;
+use crate::MAX_DEPTH;
+use gtk4 as gtk;
+
+#[derive(Debug)]
+pub struct DirEntry {
+    pub dir_path: String,
+    pub image_entries: Vec<ImageEntry>,
+}
+
+#[derive(Debug)]
+pub struct ImageEntry {
+    pub image_path: String,
+    pub image: gtk::Image,
+}
+
+impl DirEntry {
+    fn new(dir_path: String) -> Self {
+        Self {
+            dir_path,
+            image_entries: Vec::new(),
+        }
+    }
+
+    pub fn search(root: String) -> anyhow::Result<Vec<DirEntry>> {
+        let mut entries: Vec<DirEntry> = Vec::new();
+        let max_depth = count_depth(to_absolute(root.clone())?) + MAX_DEPTH;
+
+        let walker = WalkDir::new(root).into_iter();
+
+        let should_process = |entry: &walkdir::DirEntry| -> bool {
+            if let Ok(absolute) = to_absolute(entry.path()) {
+                if count_depth(absolute) > max_depth {
+                    return false;
+                }
+                if entry.file_type().is_dir() {
+                    return true;
+                }
+                return is_image(entry.path());
+            }
+            false
+        };
+
+        for entry in walker.filter_entry(should_process) {
+            let entry = entry?;
+            if entry.file_type().is_dir() {
+                continue;
+            }
+
+            let parent = entry
+                .path()
+                .parent()
+                .ok_or_else(|| anyhow!("not found parent directory"))?
+                .to_string_lossy()
+                .to_string();
+
+            let dir_entries_index = if let Some(index) = entries.iter().position(|e| e.dir_path == parent) {
+                index
+            } else {
+                entries.push(DirEntry::new(parent));
+                entries.len() - 1
+            };
+
+            entries[dir_entries_index].image_entries.push(ImageEntry {
+                image_path: entry.path().to_string_lossy().to_string(),
+                image: gtk::Image::new(),
+            });
+        }
+
+        entries.retain(|e| !e.image_entries.is_empty());
+
+        Ok(entries)
+    }
+}
+
+fn count_depth<T: ToString>(path: T) -> u32 {
+    path.to_string().chars().filter(|&c| c == path::MAIN_SEPARATOR).count() as u32
+}
+
+fn to_absolute<T: AsRef<Path>>(path: T) -> anyhow::Result<String> {
+    Ok(path::absolute(path)?.to_string_lossy().to_string())
+}
+
+fn is_image<T: AsRef<Path>>(path: T) -> bool {
+    let supported_extensions: [String; 3] = ["png".into(), "jpg".into(), "jpeg".into()];
+    let ext = path.as_ref().extension();
+    if let Some(ext) = ext {
+        supported_extensions.contains(&ext.to_string_lossy().to_string())
+    } else {
+        false
+    }
+}
