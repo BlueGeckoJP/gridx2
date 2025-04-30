6ab9352be3438c66d71a4c41b27c495563ecb4d2
bluegecko <kei09doya@gmail.com>
2025-04-30 10:49:04 +0900

    Add folder selection dialog and dynamic directory support
    Introduce a FileDialog-based folder selection capability in the UI, allowing users to choose the directory dynamically. Updated dependencies in Cargo.toml to enable new GTK features, and replaced the static directory path with a thread-safe shared state for flexibility.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 mod entry;
 
 use gtk4 as gtk;
-use gtk4::prelude::{ApplicationExt, ApplicationExtManual, BoxExt, Cast, GtkWindowExt};
-use gtk4::{gdk, glib, Application, ApplicationWindow, Picture};
+use gtk4::gio::Cancellable;
+use gtk4::prelude::{ActionMapExt, ApplicationExt, ApplicationExtManual, ApplicationWindowExt, BoxExt, Cast, FileExt, GtkApplicationExt, GtkWindowExt};
+use gtk4::{gdk, gio, glib, Application, ApplicationWindow, FileDialog, Picture};
 use image::GenericImageView;
+use std::sync::{Arc, Mutex};
 
 static MAX_DEPTH: u32 = 2;
-static BASE_DIR: &str = ".";
 static THUMBNAIL_SIZE: u32 = 200;
 
 fn main() -> glib::ExitCode {
+    let original_dir = Arc::new(Mutex::new(String::from(".")));
+
     gtk::init().expect("Failed to initialize GTK");
 
     let app = Application::builder().application_id("me.bluegecko.gridx2").build();
 
-    let d = entry::DirEntry::search(BASE_DIR.into()).unwrap();
+    let d = entry::DirEntry::search(original_dir.lock().unwrap().clone()).unwrap();
     
     app.connect_activate(move |app| {
         let window = ApplicationWindow::builder()
@@ -23,6 +26,36 @@ fn main() -> glib::ExitCode {
             .default_height(600)
             .title("gridx2")
             .build();
+
+        let menubar = gio::Menu::new();
+
+        let file_menu = gio::Menu::new();
+        file_menu.append(Some("Open Folder"), Some("app.open"));
+
+        menubar.append_submenu(Some("File"), &file_menu);
+
+        app.set_menubar(Some(&menubar));
+        window.set_show_menubar(true);
+
+        let original_dir = original_dir.clone();
+        let open_action = gio::SimpleAction::new("open", None);
+        open_action.connect_activate(glib::clone!(
+            #[weak] window,
+            move |_, _| {
+                let dialog = FileDialog::new();
+                let cancellable = Cancellable::new();
+                let original_dir = original_dir.clone();
+                dialog.select_folder(Some(&window), Some(&cancellable), move |result| {
+                    if let Ok(path) = result {
+                        if let Some(dir) = path.path() {
+                            *original_dir.lock().unwrap() = dir.to_string_lossy().to_string();
+                            println!("{:?}", original_dir.lock().unwrap());
+                        }
+                    }
+                });
+            }
+        ));
+        app.add_action(&open_action);
 
         let vbox = gtk::Box::builder().orientation(gtk::Orientation::Vertical).build();
 
